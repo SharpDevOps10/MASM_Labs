074fe89fe8d5d21b0f2d8697831220b076304dcf
SharpDevOps10 <td090833@gmail.com>
2024-02-16 23:22:39 +0200

    first added masm-32 lab

---
+++ 1-23-IM-22-Tymofeiev/1-23-IM-22-Tymofeiev-MASM32.asm
@@ -0,0 +1,25 @@
+.486
+.model flat, stdcall
+
+extern MessageBoxA@16:near
+
+includelib \masm32\lib\user32.lib
+
+data segment
+	TymofeievHead db "Лабораторна робота 1",0
+    TymofeievInfo db "ПІБ: Тимофеєв Даниіл Костянтинович",13,
+					 "Дата народження: 10.04.2005",13,
+				     "Номер залікової книжки: 8828",0
+							 
+data ends
+text segment
+start: 
+	push 0
+    push offset TymofeievHead 
+    push offset TymofeievInfo 
+	push 0
+    call MessageBoxA@16
+    
+    ret
+text ends
+end start
