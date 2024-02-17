8ce2d66cf71d3bfe37e073854d0c60451876ae2e
SharpDevOps10 <td090833@gmail.com>
2024-02-17 17:54:41 +0200

    added MASM64 lab

--- 1-23-IM-22-Tymofeiev/1-23-IM-22-Tymofeiev-MASM32.asm
+++ 1-23-IM-22-Tymofeiev/1-23-IM-22-Tymofeiev-MASM32.asm
@@ -6,7 +6,7 @@ extern MessageBoxA@16:near
 includelib \masm32\lib\user32.lib
 
 data segment
-	TymofeievHead db "Лабораторна робота 1",0
+	TymofeievHead db "Лабораторна робота 1 MASM32",0
     TymofeievInfo db "ПІБ: Тимофеєв Даниіл Костянтинович",13,
 					 "Дата народження: 10.04.2005",13,
 				     "Номер залікової книжки: 8828",0
---
+++ 1-23-IM-22-Tymofeiev/1-23-IM-22-Tymofeiev-MASM64.asm
@@ -0,0 +1,24 @@
+OPTION DOTNAME
+	
+option casemap:none
+include \masm64\include\temphls.inc
+include \masm64\include\win64.inc
+include \masm64\include\kernel32.inc
+includelib \masm64\lib\kernel32.lib
+include \masm64\include\user32.inc
+includelib \masm64\lib\user32.lib
+OPTION PROLOGUE:rbpFramePrologue
+OPTION EPILOGUE:none
+
+.data
+TymofeievHead      db 'Лабораторна робота 1 MASM64',0
+TymofeievInfo      db 'ПІБ: Тимофеєв Даниіл Костянтинович',13,
+					  'Дата народження: 10.04.2005',13,
+				      'Номер залікової книжки: 8828',0
+.code
+WinMain proc 
+	sub rsp,28h
+      invoke MessageBox, NULL, &TymofeievInfo, &TymofeievHead, MB_OK
+      invoke ExitProcess,NULL
+WinMain endp
+end
