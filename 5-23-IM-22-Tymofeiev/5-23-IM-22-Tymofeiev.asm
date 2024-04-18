0a24b7106cefa8e9fc0693b252dcab304a50c48c
SharpDevOps10 <td090833@gmail.com>
2024-04-18 23:27:10 +0300

    lab5

--- 5-23-IM-22-Tymofeiev/5-23-IM-22-Tymofeiev.asm
+++ 5-23-IM-22-Tymofeiev/5-23-IM-22-Tymofeiev.asm
@@ -1,111 +1,127 @@
 .386
 .model flat, stdcall
-option casemap:none
+option casemap :none
 
 include \masm32\include\masm32rt.inc
 
 .data
-aArray DWORD 1, 2, 3, 4, 5   ; массив значений переменной a
-bArray DWORD 2, 3, 4, 5, 6   ; массив значений переменной b
-cArray DWORD 3, 4, 5, 6, 7   ; массив значений переменной c
-resultString db 128 dup(?)   ; буфер для строки результата
-className db "MyClass", 0    ; имя класса окна
-AppName db "MyApp", 0         ; имя приложения
+    resultString db 256 dup(?)
+    titleString db "Результат", 0
 
-.data?
-hInstance HINSTANCE ?
-hWnd HWND ?
-msg MSG
+.data
+    DovzhenkoBasicTitle db "Lab 5 by Dovzhenko Anton - Example %d", 0
+    DovzhenkoDivByZeroErrorMsg db "Error! You can't divide by 0!", 0
+    DovzhenkoResultsWin db "The formula is (c/d + 3*a/2)/(c - a + 1)", 13,
+                       "a = %d, c = %d, d = %d", 13,
+                       "So the example is (%d/%d + 3*%d/2)/(%d - %d + 1)", 13,
+                       "Result before modifying: %d", 13,
+                       "Final result: %d", 0
+    DovzhenkoFourKm dd 4
+
+.data
+    DovzhenkoA dd 4, 10, 3, 7, 4
+    DovzhenkoC dd 6, 7, 48, 28, 8
+    DovzhenkoD dd 2, 7, 2, 6, 4
 
 .code
 start:
-    invoke GetModuleHandle, NULL
-    mov hInstance, eax
+    call main            ; Call the main procedure
+    invoke ExitProcess, 0  ; Exit the program
 
-    invoke WinMain, hInstance, NULL, NULL, SW_SHOWDEFAULT
+main proc
+    LOCAL DovzhenkoLpIndx: DWORD
+    
+    LOCAL DovzhenkoTitle[100]: BYTE
+    LOCAL DovzhenkoError[100]: BYTE
+    LOCAL DovzhenkoResultStr[256]: BYTE
+    
+    LOCAL DovzhenkoIntrmdRes: DWORD
+    LOCAL DovzhenkoFinalRes: DWORD
+    
+    LOCAL DovzhenkoPAVal: DWORD, DovzhenkoPCVal: DWORD, DovzhenkoPDVal: DWORD
+    LOCAL DovzhenkoNumZm: DWORD, DovzhenkoDenumZN: DWORD
 
+    mov DovzhenkoLpIndx, 0
+
+DovzhenkoCalcLoop:
+    mov eax, DovzhenkoLpIndx
+    inc eax  ; Increment index
+    
+    invoke wsprintf, addr DovzhenkoTitle, addr DovzhenkoBasicTitle , eax  ; Format the window title
+    invoke wsprintf, addr DovzhenkoError, addr DovzhenkoDivByZeroErrorMsg  ; Format the error message
+
+    mov eax, DovzhenkoLpIndx
+    shl eax, 2
+    lea ebx, [DovzhenkoA]
+    add ebx, eax
+    mov ecx, [ebx]
+    mov DovzhenkoPAVal, ecx
+
+    lea ebx, [DovzhenkoC]
+    add ebx, eax
+    mov ecx, [ebx]
+    mov DovzhenkoPCVal, ecx
+
+    lea ebx, [DovzhenkoD]
+    add ebx, eax
+    mov ecx, [ebx]
+    mov DovzhenkoPDVal, ecx
+
+    ; Calculating the numerator
+    mov eax, DovzhenkoPCVal     ; Load the value of c
+    cdq                         ; Extend eax into edx:eax
+    idiv DovzhenkoPDVal         ; Divide by d, result in edx:eax
+    mov ebx, eax                ; Save the result c/d in ebx
+
+    mov eax, DovzhenkoPAVal     ; Load the value of a
+    imul eax, 3                 ; Multiply by 3
+    sar eax, 1                  ; Divide by 2 (shift right by 1 bit)
+
+    add eax, ebx                ; Add c/d
+    mov DovzhenkoNumZm, eax     ; Save the result in the numerator
+
+    ; Calculating the denominator
+    mov eax, DovzhenkoPCVal     ; Load the value of c
+    sub eax, DovzhenkoPAVal     ; Subtract a
+    add eax, 1                  ; Add 1
+    mov DovzhenkoDenumZN, eax   ; Save the result in the denominator
+
+    ; Check for division by zero
+    cmp DovzhenkoDenumZN, 0
+    je DovzhenkoDivByZeroErr    ; Jump to error handling if the denominator is zero
+
+    ; Perform division of the numerator by the denominator
+    mov eax, DovzhenkoNumZm     ; Load the numerator
+    cdq                         ; Extend eax into edx:eax
+    idiv DovzhenkoDenumZN       ; Divide by the denominator
+    mov DovzhenkoFinalRes, eax  ; Save the result
+
+    ; Check if the result is odd or even
+    mov eax, DovzhenkoFinalRes  ; Load the final result
+    test eax, 1                  ; Check for odd/even
+    jz DovzhenkoEvenIntResult   ; If even, skip modification
+
+    imul eax, 5                  ; If odd, multiply by 5
+
+DovzhenkoEvenIntResult:
+    ; Calculate the string for the result window
+    invoke wsprintf, addr DovzhenkoResultStr, addr DovzhenkoResultsWin, DovzhenkoPAVal, DovzhenkoPCVal, DovzhenkoPDVal, DovzhenkoPCVal, DovzhenkoPDVal, DovzhenkoPAVal, DovzhenkoPCVal, DovzhenkoPAVal, DovzhenkoNumZm, DovzhenkoFinalRes
+    
+    ; Display the result window
+    invoke MessageBox, NULL, addr DovzhenkoResultStr, addr DovzhenkoTitle, MB_OK
+
+    add DovzhenkoLpIndx, 1
+    cmp DovzhenkoLpIndx, 5
+    
+    jl DovzhenkoCalcLoop
+    jmp DovzhenkoExitProcessLbl
+
+DovzhenkoDivByZeroErr:
+    invoke MessageBox, NULL, addr DovzhenkoError, addr DovzhenkoTitle, MB_ICONERROR or MB_OK
+    jmp DovzhenkoExitProcessLbl
+
+DovzhenkoExitProcessLbl:
     invoke ExitProcess, 0
-
-WinMain proc hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD
-    LOCAL wc:WNDCLASSEX
-
-    mov wc.cbSize, SIZEOF WNDCLASSEX
-    mov wc.style, CS_HREDRAW or CS_VREDRAW
-    mov wc.lpfnWndProc, OFFSET WndProc
-    mov wc.cbClsExtra, 0
-    mov wc.cbWndExtra, 0
-    push hInst
-    pop wc.hInstance
-    mov wc.hbrBackground, COLOR_WINDOW+1
-    mov wc.lpszMenuName, NULL
-    mov wc.lpszClassName, OFFSET className
-    invoke LoadIcon, NULL, IDI_APPLICATION
-    mov wc.hIcon, eax
-    mov wc.hIconSm, eax
-    invoke LoadCursor, NULL, IDC_ARROW
-    mov wc.hCursor, eax
-    invoke RegisterClassEx, addr wc
-
-    invoke CreateWindowEx, NULL, ADDR className, ADDR AppName, WS_OVERLAPPEDWINDOW, \
-           CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInst, NULL
-    mov hWnd, eax
-    invoke ShowWindow, hWnd, SW_SHOWNORMAL
-    invoke UpdateWindow, hWnd
-
-    mov esi, 0  ; начинаем с первого элемента массива
-    mov ecx, 5  ; общее количество итераций
-
-calc_loop:
-    ; Вычисление выражения
-    mov eax, DWORD PTR [aArray + esi * 4]     ; загружаем значение a
-    mov ebx, DWORD PTR [bArray + esi * 4]     ; загружаем значение b
-    mov edx, DWORD PTR [cArray + esi * 4]     ; загружаем значение c
-
-    imul eax, 3  ; вычисляем 3*a
-    shr eax, 1   ; делим на 2
-    add eax, ebx ; прибавляем к результату b
-    idiv edx     ; делим на c
-
-    push eax     ; сохраняем результат
-
-    ; Проверка четности результата
-    test eax, 1  ; проверяем младший бит
-    jz even_result  ; если четное, переходим к обработке четного результата
-
-odd_result:
-    pop eax      ; возвращаем результат обратно
-    imul eax, 5  ; умножаем на 5
-    jmp print_result
-
-even_result:
-    pop eax      ; возвращаем результат обратно
-    shr eax, 1   ; делим на 2
-
-print_result:
-    ; Печать результата в окне сообщения
-    mov edx, OFFSET resultString
-    invoke wsprintf, edx, OFFSET resultString, eax
-    invoke MessageBox, hWnd, edx, ADDR resultString, MB_OKCANCEL
-    cmp eax, IDOK  ; проверяем, что пользователь нажал "ОК"
-    je continue_loop
-
-exit:
-    invoke ExitProcess, 0
-    ret
-
-continue_loop:
-    inc esi          ; переходим к следующему набору данных
-    loop calc_loop   ; продолжаем цикл, если есть еще наборы данных
-
-WndProc proc hWnd:HWND, uMsg:UINT, wParam:WPARAM, lParam:LPARAM
-    .if uMsg == WM_DESTROY
-        invoke PostQuitMessage, 0
-    .elseif uMsg == WM_CLOSE
-        invoke DestroyWindow, hWnd
-    .else
-        invoke DefWindowProc, hWnd, uMsg, wParam, lParam
-    .endif
-    ret
-WndProc endp
+main endp
 
 end start
