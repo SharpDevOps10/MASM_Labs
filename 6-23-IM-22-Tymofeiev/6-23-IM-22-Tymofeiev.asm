85f348b136b290cc6aee38fe725fadbc7c8a14e7
SharpDevOps10 <td090833@gmail.com>
2024-05-04 20:21:49 +0300

    lab6: added the whole lab

--- 6-23-IM-22-Tymofeiev/6-23-IM-22-Tymofeiev.asm
+++ 6-23-IM-22-Tymofeiev/6-23-IM-22-Tymofeiev.asm
@@ -25,6 +25,7 @@ include \masm32\include\dialogs.inc
 	
 	TymofeievTanDominatorRes dt ?
 	TymofeievNumeratorRes dt ?
+	TymofeievTanArgument dt ?
 	
 	TymofeievARoutineString db 32 dup (?)
     TymofeievBRoutineString db 32 dup (?)
@@ -41,15 +42,19 @@ include \masm32\include\dialogs.inc
     ControlCalculationsCount dd 1
     ArrayStartPosition dd 0
     
-    Pi dq 3.141592653589793
-    TwoPi dq 6.283185307179586
+    Pi dq 3.1415926536
+    TwoPi dq 6.2831853072
     
-    TymofeievArrayForAValues dq 20.4, 22.8, 44.4, 13.8, 198.8
-    TymofeievArrayForBValues dq 0.5, 3.2, 7.2, -5.2, 49.7
-    TymofeievArrayForCValues dq -5.9, -7.8, -3.4, -7.4, 48.4
-    TymofeievArrayForDValues dq -1.7, -4.5, 5.7, 10.7, 0.3
+    TymofeievArrayForAValues dq 20.4, 22.8, 44.4, 13.8, 198.8, 6.2831853072, 6.2831853072
+    TymofeievArrayForBValues dq 0.5, 3.2, 7.2, -5.2, 49.7, 3.1415926536, -3.1415926536
+    TymofeievArrayForCValues dq -5.9, -7.8, -3.4, -7.4, 48.4, -36.7, -48.9
+    TymofeievArrayForDValues dq -1.7, -4.5, 5.7, 10.7, 0.3, 0.9, 1.5
 
-    PiOverTwo dq 1.570796326794897
+    PiOverTwo dq 1.5707963268
+	PiOverTwoMinus dq -1.5707963268
+	
+	ThreePiOverTwo dq 4.7123889804
+	ThreePiOverTwoMinus dq -4.7123889804
 
     TymofeievCheckZero dq 0.0
     TymofeievNominatorTwo dq 2.0
@@ -62,29 +67,36 @@ include \masm32\include\dialogs.inc
 	TymofeievFatalIOperationCaption db "Oops :( Fatal operation error", 0
 	TymofeievFatalIOperationBody db "Fatal math opeartion: the denominator cannot be zero :(", 0
 	
+	TymofeievFatalITanArgCaption db "Oops :( Fatal tangent argument", 0
+	
                                 
     TymofeivUndefinedTanErrorMsg db "Tangent is undefined!", 0
 
-    TymofeivVariantMessgBoxInfo db "Control calculation %d/5", 13,
+    TymofeivVariantMessgBoxInfo db "Control calculation %d/7", 13,
                     "The regular formula is (-2 * c - d * 82) / tg(a / 4 - b)", 13,
                 	"The digits are: a = %s, b = %s, c = %s, d = %s", 13,
                 	"The digital formula is (-2 * %s - %s * 82) / tg(%s / 4 - %s)", 13,     
                 	"The Eventual Result: %s", 0
 					
 					
-	TymofeivVariantInvalidMessgBoxInfo db "Control calculation %d/5", 13,
+	TymofeivVariantInvalidMessgBoxInfo db "Control calculation %d/7", 13,
                     "The regular formula is (-2 * c - d * 82) / tg(a / 4 - b)", 13,
                      "The digits are: a = %s, b = %s, c = %s, d = %s", 13,
                      "The digital formula is (-2 * %s - %s * 82) / tg(%s / 4 - %s)", 13,     
                      "Fatal math opeartion: the denominator cannot be zero :(", 0
 					 
+	TymofeivVariantUndefinedTanMessgBoxInfo db "Control calculation %d/7", 13,
+                    "The regular formula is (-2 * c - d * 82) / tg(a / 4 - b)", 13,
+                     "The digits are: a = %s, b = %s, c = %s, d = %s", 13,
+                     "The digital formula is (-2 * %s - %s * 82) / tg(%s / 4 - %s)", 13,     
+                     "Fatal tg opeartion: tangent is not defined in Pi/2 + Pi*k", 0
+					 
 .code 
 
 TymofeievOutputNormalMSGBoxLoop PROC uses ebx edi
 
-    ; Update variables for display
-    invoke FloatToStr, [TymofeievArrayForAValues + edi * 8], offset TymofeievARoutineString
-    invoke FloatToStr, [TymofeievArrayForBValues + edi * 8], offset TymofeievBRoutineString
+    invoke FloatToStr2, [TymofeievArrayForAValues + edi * 8], offset TymofeievARoutineString
+    invoke FloatToStr2, [TymofeievArrayForBValues + edi * 8], offset TymofeievBRoutineString
     invoke FloatToStr, [TymofeievArrayForCValues + edi * 8], offset TymofeievCRoutineString
     invoke FloatToStr, [TymofeievArrayForDValues + edi * 8], offset TymofeievDRoutineString
 
@@ -122,6 +134,11 @@ TymofeievTangentProcedure ENDP
 
 TymofeievHandleErrorMessage PROC uses ebx edi
 
+    invoke FloatToStr2, [TymofeievArrayForAValues + edi * 8], offset TymofeievARoutineString
+    invoke FloatToStr2, [TymofeievArrayForBValues + edi * 8], offset TymofeievBRoutineString
+    invoke FloatToStr, [TymofeievArrayForCValues + edi * 8], offset TymofeievCRoutineString
+    invoke FloatToStr, [TymofeievArrayForDValues + edi * 8], offset TymofeievDRoutineString
+	
     ; Formatting an Error Message
     invoke wsprintf, offset TymofeievSummaryBuffer, offset TymofeivVariantInvalidMessgBoxInfo,
        ebx,
@@ -142,21 +159,53 @@ start:
 
 TymofeievCountTheFormulaLoop:
 
-    cmp edi, 5
+    cmp edi, 7
     je TymofeievEscapeLoop
     
     FINIT
 
-    ; Compute tg(a / 4 - b)
+    ; Compute (a / 4 - b) and store it in TymofeievTanArgument
     FLD [TymofeievArrayForAValues + edi * 8]
     FDIV TymofeievForTanFour
     FSUB [TymofeievArrayForBValues + edi * 8]
-    FPTAN ; Calculate tangent
+    FSTP TymofeievTanArgument
+	
+	; Check if (a / 4 - b) equals PiOverTwoMinus
+    FLD TymofeievTanArgument
+    FCOMP PiOverTwoMinus
+    FSTSW ax
+    SAHF
+    JE TymofeievUndefinedTanArgumentMSGBoxLoop
+	
+	; Check if (a / 4 - b) equals PiOverTwo
+	FLD TymofeievTanArgument
+    FCOMP PiOverTwo
+    FSTSW ax
+    SAHF
+    JE TymofeievUndefinedTanArgumentMSGBoxLoop
+	
+	; Check if (a / 4 - b) equals ThreePiOverTwo
+	FLD TymofeievTanArgument
+    FCOMP ThreePiOverTwo
+    FSTSW ax
+    SAHF
+    JE TymofeievUndefinedTanArgumentMSGBoxLoop
+	
+	; Check if (a / 4 - b) equals ThreePiOverTwoMinus
+	FLD TymofeievTanArgument
+    FCOMP ThreePiOverTwoMinus
+    FSTSW ax
+    SAHF
+    JE TymofeievUndefinedTanArgumentMSGBoxLoop
+	
+	; Calculate the tangent only if (a / 4 - b) is not equal to (PiOverTwo | PiOverTwoMinus) OR (ThreePiOverTwo | ThreePiOverTwoMinus)
+    FLD TymofeievTanArgument
+    FPTAN
     FSTP st(0) ; Discard the tangent result, we need only the division result
     FSTP TymofeievTanDominatorRes
 
 
-    ; Check if the dominator with tg is undefined
+    ; Check if the dominator with tg is 0
     FLD TymofeievTanDominatorRes
     FCOMP TymofeievCheckZero
     FSTSW ax
@@ -187,6 +236,12 @@ TymofeievCountTheFormulaLoop:
     jmp TymofeievCountTheFormulaLoop
 
 TymofeievOutputInvalidMSGBoxLoop:
+
+    invoke FloatToStr2, [TymofeievArrayForAValues + edi * 8], offset TymofeievARoutineString
+    invoke FloatToStr2, [TymofeievArrayForBValues + edi * 8], offset TymofeievBRoutineString
+    invoke FloatToStr, [TymofeievArrayForCValues + edi * 8], offset TymofeievCRoutineString
+    invoke FloatToStr, [TymofeievArrayForDValues + edi * 8], offset TymofeievDRoutineString
+	
     invoke wsprintf, offset TymofeievSummaryBuffer, offset TymofeivVariantInvalidMessgBoxInfo,
        ebx,
        offset TymofeievARoutineString, offset TymofeievBRoutineString, offset TymofeievCRoutineString, offset TymofeievDRoutineString,
@@ -196,6 +251,24 @@ TymofeievOutputInvalidMSGBoxLoop:
 	inc edi
 	inc ebx ; Increment window title counter
     jmp TymofeievCountTheFormulaLoop   
+	
+	
+TymofeievUndefinedTanArgumentMSGBoxLoop:
+
+    invoke FloatToStr2, [TymofeievArrayForAValues + edi * 8], offset TymofeievARoutineString
+    invoke FloatToStr2, [TymofeievArrayForBValues + edi * 8], offset TymofeievBRoutineString
+    invoke FloatToStr, [TymofeievArrayForCValues + edi * 8], offset TymofeievCRoutineString
+    invoke FloatToStr, [TymofeievArrayForDValues + edi * 8], offset TymofeievDRoutineString
+	
+    invoke wsprintf, offset TymofeievSummaryBuffer, offset TymofeivVariantUndefinedTanMessgBoxInfo,
+       ebx,
+       offset TymofeievARoutineString, offset TymofeievBRoutineString, offset TymofeievCRoutineString, offset TymofeievDRoutineString,
+       offset TymofeievCRoutineString, offset TymofeievDRoutineString, offset TymofeievARoutineString, offset TymofeievBRoutineString
+    
+    invoke MessageBox, NULL, offset TymofeievSummaryBuffer, offset TymofeievFatalITanArgCaption, MB_OK
+	inc edi
+	inc ebx ; Increment window title counter
+    jmp TymofeievCountTheFormulaLoop 
 
 
 TymofeievEscapeLoop:
